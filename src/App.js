c4d74d71a2344275019c75e4adef680f746ddd07
leloufadel <leloufadel@gmail.com>
2023-07-21 20:25:37 +0300

    add list book container

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Book from './components/Book/Book';
+
+function App() {
+  return (
+    <Book />
+  );
+}
+
+export default App;
---
+++ src/components/Book/CircularProgress.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './CircularProgress.module.css';
+
+const CircularProgress = () => (
+  <div className={classes.circularProgressContainer}>
+    <div className={classes.circularProgress} />
+  </div>
+);
+
+export default CircularProgress;
