c2e7cdfe0c3af4242672d9c8e619c75e9053b3fc
leloufadel <leloufadel@gmail.com>
2023-07-27 14:01:39 +0300

    create Book style

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,16 +1,16 @@
 // File: src/components/Book/Book.jsx
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { removeBook } from '../../redux/books/booksSlice';
+// import { useDispatch } from 'react-redux';
+// import { removeBook } from '../../redux/books/booksSlice';
 import classes from './Book.module.css';
 import CircularProgress from './CircularProgress';
 
-const Book = ({ book }) => {
-  const dispatch = useDispatch();
-  const handleRemoveBook = (id) => {
-    dispatch(removeBook(id));
-  };
+// const Book = ({ book }) => {
+//   const dispatch = useDispatch();
+//   const handleRemoveBook = (id) => {
+//     dispatch(removeBook(id));
+//   };
 
   return (
     <li className={classes.listContainer}>
@@ -22,7 +22,8 @@ const Book = ({ book }) => {
         </div>
         <div className={classes.actionBtn}>
           <button type="button">Comments</button>
-          <button onClick={() => handleRemoveBook(book.item_id)} type="button">
+          <button>
+          {/* <button onClick={() => handleRemoveBook(book.item_id)} type="button"> */}
             Remove
           </button>
           <button type="button">Edit</button>
@@ -40,15 +41,15 @@ const Book = ({ book }) => {
       </div>
     </li>
   );
-};
+// };
 
-Book.propTypes = {
-  book: PropTypes.shape({
-    category: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
-    item_id: PropTypes.number.isRequired,
-  }).isRequired,
-};
+// Book.propTypes = {
+//   book: PropTypes.shape({
+//     category: PropTypes.string.isRequired,
+//     title: PropTypes.string.isRequired,
+//     author: PropTypes.string.isRequired,
+//     item_id: PropTypes.number.isRequired,
+//   }).isRequired,
+// };
 
 export default Book;
