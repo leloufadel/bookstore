d825a803b09c8cbeafe248158efae12f580c5766
leloufadel <leloufadel@gmail.com>
2023-07-27 14:14:35 +0300

    Use Redux in React components

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,16 +1,15 @@
-// File: src/components/Book/Book.jsx
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { useDispatch } from 'react-redux';
-// import { removeBook } from '../../redux/books/booksSlice';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../../redux/books/booksSlice';
 import classes from './Book.module.css';
 import CircularProgress from './CircularProgress';
 
-// const Book = ({ book }) => {
-//   const dispatch = useDispatch();
-//   const handleRemoveBook = (id) => {
-//     dispatch(removeBook(id));
-//   };
+const Book = ({ book }) => {
+  const dispatch = useDispatch();
+  const handleRemoveBook = (id) => {
+    dispatch(removeBook(id));
+  };
 
   return (
     <li className={classes.listContainer}>
@@ -22,8 +21,8 @@ import CircularProgress from './CircularProgress';
         </div>
         <div className={classes.actionBtn}>
           <button type="button">Comments</button>
-          <button>
-          {/* <button onClick={() => handleRemoveBook(book.item_id)} type="button"> */}
+
+          <button onClick={() => handleRemoveBook(book.item_id)} type="button">
             Remove
           </button>
           <button type="button">Edit</button>
@@ -41,15 +40,15 @@ import CircularProgress from './CircularProgress';
       </div>
     </li>
   );
-// };
+};
 
-// Book.propTypes = {
-//   book: PropTypes.shape({
-//     category: PropTypes.string.isRequired,
-//     title: PropTypes.string.isRequired,
-//     author: PropTypes.string.isRequired,
-//     item_id: PropTypes.number.isRequired,
-//   }).isRequired,
-// };
+Book.propTypes = {
+  book: PropTypes.shape({
+    category: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    item_id: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default Book;
