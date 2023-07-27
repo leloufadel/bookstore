1868a78ee55971923627161f8947faea4e78c9b5
leloufadel <leloufadel@gmail.com>
2023-07-27 10:20:59 +0300

    add reducers and actions

--- src/components/BookLists/BookList.jsx
+++ src/components/BookLists/BookList.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import classes from './BookList.module.css';
 import Book from '../Book/Book';
 
-const BookList = () => (
-  <ul className={classes.bookListContainer}>
-    <Book />
-    <Book />
-  </ul>
-);
+const BookList = () => {
+  const { books } = useSelector((state) => state.book);
 
+  return (
+    <ul className={classes.bookListContainer}>
+      {books.map((book) => (
+        <Book key={book.item_id} book={book} />
+      ))}
+    </ul>
+  );
+};
 export default BookList;
